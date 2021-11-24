f56508e6f4c43cdd62d8de34298c5fe2d6b586b6
Conner Martin <coma6440@colorado.edu>
2021-11-23 19:14:21 -0700

    more plots

--- main.m
+++ main.m
@@ -6,6 +6,7 @@ clc
 %Correlation coefficients
 %Statistical tests for residuals
 %Start gathering results
+% Get daily maxs/mins
 %%
 % A = readtable('output/WIND_DIRECTION.csv');
 % B = readtable('output/HORIZONTAL_WIND_SPEED.csv');
@@ -22,11 +23,11 @@ pressure = sortrows(readtable("output/pressure_stats.csv", 'MissingRule', 'omitr
 humidity = sortrows(readtable("output/humidity_stats.csv", 'MissingRule', 'omitrow'));
 
 %% Initial Plots of Results
-f = plot_summary(temperature, "Daily Temperature Summary");
+f = plot_summary(temperature);
 saveas(f, "images/t_summary_init.png")
-f = plot_summary(pressure, "Daily Pressure Summary");
+f = plot_summary(pressure);
 saveas(f, "images/p_summary_init.png")
-f = plot_summary(humidity, "Daily Humidity Summary");
+f = plot_summary(humidity);
 saveas(f, "images/h_summary_init.png")
 %% Remove Outliers
 temperature = rmoutliers(temperature);
@@ -58,11 +59,11 @@ saveas(f, "images/p_fit.png")
 [~, ~, f] = plot_fit(idx, humidity, h_min_fit, h_max_fit, "Humidity [%]");
 saveas(f, "images/h_fit.png")
 
-f = resid_histogram(t_min_out, t_max_out, "Tempearature Fit Residuals");
+f = resid_histogram(t_min_out, t_max_out);
 saveas(f, "images/t_resid_hist.png")
-f = resid_histogram(p_min_out, p_max_out, "Pressure Fit Residuals");
+f = resid_histogram(p_min_out, p_max_out);
 saveas(f, "images/p_resid_hist.png")
-f = resid_histogram(h_min_out, h_max_out, "Humidity Fit Residuals");
+f = resid_histogram(h_min_out, h_max_out);
 saveas(f, "images/h_resid_hist.png")
 
 
@@ -91,17 +92,34 @@ saveas(f, "images/p_periodogram.png")
 [f, h_peaks_min, h_peaks_max] = periodogram_plots(h_min, h_max, 1);
 saveas(f, "images/h_periodogram.png")
 
-%% Delta T
-figure
-scatter(temperature.SOL, temperature.MAXIMUM - temperature.MINIMUM, '.')
-axis tight
+%% Correlation between variables
+idx = intersect(intersect(temperature.SOL, pressure.SOL), humidity.SOL);
+t_idx = zeros(length(idx),1);
+p_idx = zeros(length(idx),1);
+h_idx = zeros(length(idx),1);
 
+for i = 1:length(idx)
+    t_idx(i) = find(temperature.SOL == idx(i));
+    p_idx(i) = find(pressure.SOL == idx(i));
+    h_idx(i) = find(humidity.SOL == idx(i));
+end
+
+data = [t_min(t_idx), t_max(t_idx), p_min(p_idx), p_max(p_idx), h_min(h_idx), h_max(h_idx)];
+R = corrcoef(data);
+tick_labels = ["Min(T)","Max(T)","Min(P)","Max(P)","Min(H)","Max(H)"];
+f = figure('Visible','off');
+h = heatmap(R);
+ax = gca;
+ax.XDisplayLabels = tick_labels;
+ax.YDisplayLabels = tick_labels;
+saveas(f, "images/var_corr.png")
 %% Plots After Further Cleaning
 % plot_summary(temperature, "Daily Temperature Summary");
 % plot_summary(pressure, "Daily Pressure Summary");
 % plot_summary(humidity, "Daily Humidity Summary");
 %% Helper Functions
-function f = plot_summary(A, title_str)
+function f = plot_summary(A)
+A = rmoutliers(A);
 f = figure('Visible','off');
 nexttile
 scatter(A.SOL, A.MINIMUM, '.')
@@ -127,7 +145,6 @@ nexttile
 scatter(A.SOL, A.STD, '.')
 xlabel("Sol")
 ylabel("STANDARD DEVIATION")
-sgtitle(title_str)
 end
 
 function [min_fit, max_fit, f] = plot_fit(t, data, min_fit, max_fit, yaxis_str)
@@ -154,22 +171,23 @@ axis tight
 title("Maximum")
 end
 
-function f = resid_histogram(min_out, max_out, title_str)
+function f = resid_histogram(min_out, max_out)
 f = figure('Visible','off');
 nexttile
-histogram(min_out.residuals, 'Normalization', 'probability')
-ylabel("Probability")
+histogram(min_out.residuals)
+ylabel("Counts")
 xlabel("Residual Value")
+title("Minimum")
 nexttile
-histogram(max_out.residuals, 'Normalization', 'probability')
-ylabel("Probability")
+histogram(max_out.residuals)
+ylabel("Counts")
 xlabel("Residual Value")
-sgtitle(title_str)
+title("Maximum")
 end
 
 function f = cwt_plots(t, d_min, d_max)
-[cmin,fmin] = cwt(d_min);
-[cmax,fmax] = cwt(d_max);
+[cmin,fmin] = cwt(detrend(d_min));
+[cmax,fmax] = cwt(detrend(d_max));
 
 f = figure('Visible','off');
 tiledlayout(1,2)
--- wind_speed.m
+++ wind_speed.m
@@ -8,15 +8,16 @@ h_ws = sortrows(readtable("output/HORIZONTAL_WIND_SPEED.csv", 'MissingRule', 'om
 ws = h_ws.Var5;
 t = h_ws.Var1;
 h_wd = sortrows(readtable("output/WIND_DIRECTION.csv", 'MissingRule', 'omitrow'));
-%G = groupsummary(h_ws, 'Var1', 'mean', 'Var5')
+
 %% Fitting a Distribution
 pd_wb = fitdist(ws, 'Weibull');
 pd_nm = fitdist(ws, 'Normal');
 pd_gm = fitdist(ws, 'Gamma');
 
 %% QQ Plot
-figure
+f = figure('Visible','off');
 qqplot(ws)
+saveas(f, "images/ws_qq.png")
 
 %% Goodness of fit
 [h_nm,p_nm] = chi2gof(ws);
@@ -30,7 +31,7 @@ ws_bs = datasample(ws,1000);
 
 %% Plotting distribution and visualizing
 s_test = 0:0.1:20;
-figure
+f = figure('Visible','off');
 histogram(ws, 'Normalization', 'pdf')
 hold on
 plot(s_test, pdf(pd_wb,s_test), 'LineWidth',2)
@@ -39,31 +40,144 @@ plot(s_test, pdf(pd_gm,s_test), 'LineWidth', 2)
 xlabel("Wind Speed [m/s]")
 ylabel("Probability Density")
 legend("Data", "Weibull Fit", "Normal Fit", "Gamma Fit")
-
+saveas(f, "images/ws_fitted.png")
 
 %% Plotting variation with moving average
-figure
+f = figure('Visible','off');
 hold on
 grid on
 scatter(t, ws,'.')
-plot(t, smooth(ws,75), 'LineWidth', 2)
+plot(t, smooth(ws,100), 'LineWidth', 2)
 xlabel("Sol")
 ylabel("Wind Speed [m/s]")
 legend("Data", "Moving average")
+saveas(f, "images/ws_time.png")
 
-%% Time Evolution of Wind Direction
-%Histogram not working well due to size of measurements, need to find way
-%to remedy. Let's try bootstrapping by sol
-G = groupsummary(h_wd, 'Var1', 'mean', 'Var5');
-figure
-hold on
-scatter(h_wd.Var1, h_wd.Var5,'.')
+%% More Wind
+sols = h_wd.Var1;
+t_wd = h_wd.Var2;
+wd = h_wd.Var5;
+n = length(h_wd.Var1);
+load('wd_time.mat');
+% for i = 1:n
+%     t_str = h_wd.Var3{i};
+%     time(i) = datetime(t_str(7:end),'Format','HH:MM:ss');
+% end
+
+G = groupsummary(h_wd, 'Var1', @(x) ang_mean(x), 'Var5');
+t_mean = G.Var1;
+wd_mean = rad2deg(mod(G.fun1_Var5,2*pi));
+
+f = figure('Visible','off');
+scatter(sols,wd,'.')
 axis tight
-plot(G.Var1, G.mean_Var5)
-plot(G.Var1, smooth(G.mean_Var5, 75),'LineWidth', 2)
 xlabel("Sol")
-ylabel("Wind Direction [Degree]")
-legend("Raw Data", "Sol Averaged Data", "Smoothed, Averaged Data")
-%Makes sense for wind-drection to be primarily coming from the south,
-%curiosity is located in southern hemisphere https://marsed.asu.edu/mep/wind
-%Additionally shows significant South EAST !
+ylabel("Wind Direction [Degrees]")
+hold on
+scatter(t_mean, wd_mean,'.')
+saveas(f, "images/wd_time.png")
+
+
+f = figure('Visible','off');
+histogram(time,20)
+datetick('x','HH:MM:SS')
+xlabel("LMST")
+ylabel("Observations")
+axis tight
+saveas(f, "images/wd_obs_times.png")
+
+%Majority of samples taken during the day
+
+f = figure('Visible','off');
+polarhistogram(deg2rad(wd))
+pax = gca;
+pax.ThetaZeroLocation = 'top';
+pax.ThetaDir = 'clockwise';
+angles = 0:45:360;
+pax.ThetaTick = angles;
+labels = {'N','NE','E','SE','S','SW','W','NW'};
+pax.ThetaTickLabel = labels;
+saveas(f, "images/wd_histogram.png")
+
+%% Splitting Wind-Direction by Time
+t_rise = datetime('05:00:00','Format','HH:MM:ss');
+t_fall = datetime('21:00:00','Format','HH:MM:ss');
+day = time > t_rise & time < t_fall;
+night = time < t_rise | time > t_fall;
+
+f = figure('Visible','off');
+polarhistogram(deg2rad(wd(day)))
+pax = gca;
+pax.ThetaZeroLocation = 'top';
+pax.ThetaDir = 'clockwise';
+angles = 0:45:360;
+pax.ThetaTick = angles;
+labels = {'N','NE','E','SE','S','SW','W','NW'};
+pax.ThetaTickLabel = labels;
+saveas(f, "images/wd_histogram_day.png")
+
+f = figure('Visible','off');
+polarhistogram(deg2rad(wd(night)))
+pax = gca;
+pax.ThetaZeroLocation = 'top';
+pax.ThetaDir = 'clockwise';
+angles = 0:45:360;
+pax.ThetaTick = angles;
+labels = {'N','NE','E','SE','S','SW','W','NW'};
+pax.ThetaTickLabel = labels;
+saveas(f, "images/wd_histogram_night.png")
+
+%% Looking at Wind Variability
+edges = 22.5:45:360;
+edges = [0, edges, 360];
+u_sols = unique(sols);
+m = length(unique(sols));
+counts = zeros(m,9);
+for i = 1:m
+    dat = h_wd.Var5(h_wd.Var1 == u_sols(i));
+    counts(i,:) = histcounts(dat,edges); 
+end
+counts(:,1) = counts(:,1) + counts(:,9);
+counts(:,9) = [];
+
+max_c = max(counts,[],'all');
+
+figure
+tiledlayout(4,2)
+nexttile
+scatter(u_sols, counts(:,1),'.');
+ylim([0,max_c]);
+nexttile
+scatter(u_sols, counts(:,2),'.');
+ylim([0,max_c]);
+
+nexttile
+scatter(u_sols, counts(:,3),'.');
+ylim([0,max_c]);
+
+nexttile
+scatter(u_sols, counts(:,4),'.');
+ylim([0,max_c]);
+
+nexttile
+scatter(u_sols, counts(:,5),'.');
+ylim([0,max_c]);
+
+nexttile
+scatter(u_sols, counts(:,6),'.');
+ylim([0,max_c]);
+
+nexttile
+scatter(u_sols, counts(:,7),'.');
+ylim([0,max_c]);
+
+nexttile
+scatter(u_sols, counts(:,8),'.');
+ylim([0,max_c]);
+
+
+
+%% Helper Functions
+function out = ang_mean(x)
+out = atan2(mean(sin(x)),mean(cos(x)));
+end
